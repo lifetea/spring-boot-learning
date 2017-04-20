100ce89897adf4cdfaafb19d04b444404e531d63
lifetea <494886251@qq.com>
2017-04-20 22:40:11 +0800

    fixs #1

---
+++ src/main/java/com/lifetea/model/Links.java
@@ -0,0 +1,56 @@
+package com.lifetea.model;
+
+import javax.persistence.*;
+
+public class Links extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "SELECT LAST_INSERT_ID()")
+    private Integer id;
+
+    private String url;
+
+    @Column(name = "userId")
+    private Integer userid;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @param url
+     */
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+
+    /**
+     * @return userId
+     */
+    public Integer getUserid() {
+        return userid;
+    }
+
+    /**
+     * @param userid
+     */
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+}
---
+++ src/main/java/com/lifetea/model/User.java
@@ -0,0 +1,39 @@
+package com.lifetea.model;
+
+import javax.persistence.*;
+
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "SELECT LAST_INSERT_ID()")
+    private Integer id;
+
+    private String name;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+}
